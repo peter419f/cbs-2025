77b906f7382ea11edf5823cde36edd1e8e9bc240
miwnKEA <mwn.digi@cbs.dk>
2025-09-08 11:22:51 +0200

    Update app.js

--- 2/vejr-app/app.js
+++ 2/vejr-app/app.js
@@ -11,8 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/vejr/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/vejr', indexRouter);
+app.use('/', indexRouter);
 
 module.exports = app;
